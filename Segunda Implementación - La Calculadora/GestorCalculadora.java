ac80eb5da176ca6da0fd2db274a6095cb0ecdc73
VicenteMazzoni <vicemanya1891@gmail.com>
2025-08-15 22:37:28 -0300

    Add files via upload

---
+++ Segunda Implementación - La Calculadora/GestorCalculadora.java
@@ -0,0 +1,10 @@
+package CalculadoraVnt;
+
+public class GestorCalculadora {
+
+	public static void main(String[] args) {
+		vtnCalculadora vtnCalcu = new vtnCalculadora();
+			vtnCalcu.setVisible(true);
+	}
+
+}
